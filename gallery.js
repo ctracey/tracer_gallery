b7d103bc4f7508b3b55a94c8885fda91ec58cb3b
tracer <tracey.chris@gmail.com>
2019-08-24 23:42:59 +1000

    Hidden logging console

--- gallery.js
+++ gallery.js
@@ -1,5 +1,7 @@
 // Run this function after the page has loaded
 $(() => {
+  $('#logs').append("<li>gallery js test</li>");
+
   //Add sample images
   $('#gallery_container').append("<img style='width:100%' src='./test/sample_images/sample1.png'/>");
   $('#gallery_container').append("<img style='width:100%' src='./test/sample_images/sample2.jpg'/>");
