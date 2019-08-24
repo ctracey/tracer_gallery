401b08c44c72e8e841e6bbc54c3b00f1727dd11e
tracer <tracey.chris@gmail.com>
2019-08-24 23:41:08 +1000

    Dynamically build gallery

---
+++ gallery.js
@@ -0,0 +1,11 @@
+// Run this function after the page has loaded
+$(() => {
+  //Add sample images
+  $('#gallery_container').append("<img style='width:100%' src='./test/sample_images/sample1.png'/>");
+  $('#gallery_container').append("<img style='width:100%' src='./test/sample_images/sample2.jpg'/>");
+  $('#gallery_container').append("<img style='width:100%' src='./test/sample_images/sample3.jpg'/>");
+  $('#gallery_container').append("<img style='width:100%' src='./test/sample_images/sample4.jpg'/>");
+  $('#gallery_container').append("<img style='width:100%' src='./test/sample_images/sample5.jpg'/>");
+  $('#gallery_container').append("<img style='width:100%' src='./test/sample_images/sample6.jpg'/>");
+})
+
