961cad43a2f7dafd5151cbab8e3306115eb466dd
tracer <tracey.chris@gmail.com>
2019-08-26 23:37:55 +1000

    Select limited set of random images from folder

--- main.js
+++ main.js
@@ -59,27 +59,26 @@ app.on('activate', function () {
 // code. You can also put them in separate files and require them here.
 
 ipc.on('select-gallery-images', function (event, eventData) {
-  console.log('\nEVENT: select-gallery-images');
-  console.log('eventData: ' + eventData);
+  logEventReceived('select-gallery-images', eventData)
 
   console.log('selecting images');
   var galleryFolder = eventData['galleryFolder'];
 
   try {
     fs.readdir(galleryFolder, function(err, filenames) {
+      console.log('folder images:')
       console.log(filenames);
 
-      var selectedImages = [];
+      //check if enough images are in folder
       var setSize = filenames.length < defaultGallerySetSize ? filenames.length : defaultGallerySetSize;
-      for (var i=0; i<setSize; i++) {
-        selectedImages.push(filenames[i]);
-      }
+      //select random images from folder
+      var selectedImages = selectRandomImages(filenames, setSize)
 
       mainWindow.webContents.send('gallery-images-selected', {
         'galleryFolder': galleryFolder,
         'imageFilenames': selectedImages
       });
-      console.log('TRIGGERED EVENT: gallery-images-selected');
+      logEventTriggered('gallery-images-selected')
     });
   }
   catch (err) {
@@ -88,16 +87,65 @@ ipc.on('select-gallery-images', function (event, eventData) {
 })
 
 ipc.on('gallery-loaded', function (event, eventData) {
-  console.log('\nEVENT: gallery-loaded');
-  console.log('eventData: ' + eventData);
+  logEventReceived('gallery-loaded', eventData)
 
   try {
     mainWindow.webContents.send('init-gallery', {
       'defaultFolder': defaultFolder
     });
-    console.log('TRIGGERED EVENT: init-gallery');
+    logEventTriggered('init-gallery')
   }
   catch (err) {
     console.log(err);
   }
 })
+
+function selectRandomImages(filenames, setSize) {
+  var index = 0;
+  var selectedImages = []
+
+  var shuffledIndexes = shuffleArray(Array.from(Array(filenames.length).keys()))
+
+  //until required images selected or no more files left to choose from
+  while (selectedImages.length < setSize && index < shuffledIndexes.length) {
+    //select valid images in shuffled order
+    var imageFilename = filenames[shuffledIndexes[index]]
+      if (imageFilename.match(/\.(gif|jpg|jpeg|tiff|png)$/) != null) {
+        selectedImages.push(filenames[shuffledIndexes[index]]);
+      }
+    index++;
+  }
+
+  console.log('selected images:')
+  console.log(selectedImages)
+
+  return selectedImages
+}
+
+function shuffleArray(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array
+}
+
+function logEventReceived(eventName, eventData) {
+  console.log('\nEVENT: ' + eventName);
+  console.log('eventData: ' + eventData);
+}
+
+function logEventTriggered(eventName) {
+  console.log('TRIGGERED EVENT: ' + eventName);
+}
